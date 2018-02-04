0a3cc75599b1438b3b1abc2ac53a694b2882e153
ryandwalters <ryandwalters@gmail.com>
2018-02-04 11:41:53 -0600

    Add files via upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,79 @@
+
+//Random computer score generated between 19 and 120
+//variable is computerScore = math.floor(removes decimal & rounds to nearest whole number) math.random (generates random decimal and multiplies it by ) 
+    var computerScore = Math.floor(Math.random() *120) + 1; 
+    console.log("Computer Pick: " + computerScore );
+
+    var Wins = 0;
+    var Losses = 0;
+// On go a different random number is assigned to each crystal between 1 - 12. Each time a new game starts a new random number is generated.
+//
+//for (i = 0; i<4; i++); {
+  //  var random = math.floor(math.random() * 12);
+//    var buttons = $('<div>');
+  //      crystal.attr("class", 'crysal');
+
+//+ " button1: " + button1 + " button2: " + button2 + " button3: " + button3 + " button4: " + button4);
+
+
+
+//if playerScore === computerscore Wins++; else if playerScore !== computerScore losses++
+
+               // (document).ready(function() {
+
+                    // Here we use jQuery to select the header with "click-me" as its ID.
+                    // Whenever it is clicked...
+                   // $("#click-me").on("click", function() {
+                    
+                      // ... we trigger an alert.
+                     // alert("I've been clicked!");
+
+                    //$("#button1").on("click", function() {
+    //alert("I've been clicked!"});
+                    //)};
+                        // Here this lockGame line prevents the user from changing the option after the game is done.
+        
+                
+                          // We get the value associated with the button the user picked from here
+    //button1 = $(this).attr("value");
+                      //  }
+        
+    
+
+        
+    
+                
+    
+      // We then reveal the computer's pick in the html
+      //$("#computer-pick").text(computerPick);
+    
+    
+                    //
+                    //var valBetween = function;
+                    //function valBetween(120, 19, 120) {
+                    //  return (Math.min(120, Math.max(19, 120)));
+                    //}
+                    //console.log(valBetween);
+    
+                    //$("computerScore") = Math.miMath.floor(Math.random() * 4) + 1;
+                    //console.log("Computer Pick: " + computerScore);
+    
+
+
+    
+                    //$("#button1").on("click", function () {
+    
+                        // ... we generate a random number
+    
+                       // var button1 = Math.floor(Math.random() * 12) + 1;
+                        // ... and then dump the random number into our random-number div.
+                
+    
+    
+    // click on crystals adds to PlayScore block
+    
+
+    
+    
+    //if button value = another value in the array, select a new number
+    
